c089895a81cd21f076d3c595e286fa96676dbe93
Mansur550 <mansurislam59@gmail.com>
2026-01-14 20:52:19 +0600

    Validation Pipes added in signup dto

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,8 +1,15 @@
+import { IsAlpha, IsAlphanumeric, IsEmail, IsString, Matches, MinLength } from "class-validator";
 
 export class SignupDto {
+    @IsString()
     name: string;
 
+    @IsEmail()
     email: string;
 
+    @IsString()
+    @IsAlphanumeric()
+    @MinLength(6)
+    @Matches(/^(?=.[0-9])/, { message: 'Password must contain character and at least one number' })
     password: string;
 }
