c78d4044624b2f0c9d7a3b03c0ff610eaf894515
Mansur550 <mansurislam59@gmail.com>
2026-01-15 04:27:27 +0600

    jwt config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '1234',
-    database: 'moderator',
-    autoLoadEntities: true,
-    synchronize: true
-  })
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    cache: true,
+    load: [config],
+
+  }),
+    AuthModule, TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234',
+      database: 'moderator',
+      autoLoadEntities: true,
+      synchronize: true
+    })
 
   ],
   controllers: [AppController,],
